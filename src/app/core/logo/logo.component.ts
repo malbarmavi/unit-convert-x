1e2536ca468a1ed66ed54e62dc97819fc9e766c3
muhammed albarmavi <muhammed.albarmavi@gmail.com>
2024-04-17 21:49:40 +0300

    create logo component

---
+++ src/app/core/logo/logo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'uc-logo',
+  standalone: true,
+  template: `<span>Unit Convert</span> <span class="f-light f-xl">X</span>`,
+  styles: ``,
+})
+export class LogoComponent {}
