40eefe8bfbfe4051cf97aaedb97e72f42506306e
muhammed albarmavi <muhammed.albarmavi@gmail.com>
2024-04-20 16:20:16 +0300

    run eslint

--- src/app/core/util/test.ts
+++ src/app/core/util/test.ts
@@ -1,5 +1,6 @@
 export abstract class Test {
   static spy<T, K extends keyof T>(object: T, method: K) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     return spyOn<T>(object, method as any) as jasmine.Spy<any>;
   }
 }
