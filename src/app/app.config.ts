bca7a9d5e78ab026d1e2b4a374e27704d7538af4
muhammed albarmavi <muhammed.albarmavi@gmail.com>
2024-04-20 20:16:45 +0300

    pwa ⚡

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { errorHandlerInterceptor } from '@core/interceptors/error-handler.interceptor';
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(withInterceptors([errorHandlerInterceptor])),
-  ],
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 };
