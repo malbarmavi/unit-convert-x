48d1a27900e0089e70bc84cbf6c1e1e6ab365b28
muhammed albarmavi <muhammed.albarmavi@gmail.com>
2024-06-07 09:42:49 +0300

    fix ClickEffectDirective test

--- src/app/core/directives/click-effect.directive.spec.ts
+++ src/app/core/directives/click-effect.directive.spec.ts
@@ -1,8 +1,16 @@
+import { TestBed } from '@angular/core/testing';
 import { ClickEffectDirective } from './click-effect.directive';
 
 describe('ClickEffectDirective', () => {
+  let directive: ClickEffectDirective;
+
+  beforeEach(() => {
+    TestBed.runInInjectionContext(() => {
+      directive = new ClickEffectDirective();
+    });
+  });
+
   it('should create an instance', () => {
-    const directive = new ClickEffectDirective();
     expect(directive).toBeTruthy();
   });
 });
